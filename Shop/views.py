c7cf1a07544f61c17d45b369e882ac3f089802f1
E-zn131 <eyadzanaty131@gmail.com>
2025-02-09 01:20:54 +0200

    Finishing Shop-Category

--- Shop/views.py
+++ Shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from Home.models import Products, Cart
 from .filters import ProductFilter
 # Create your views here.
@@ -10,5 +10,15 @@ def category(request):
     top_orders = Products.get_largest_orders(5)
     filter= ProductFilter(request.GET, queryset=products_filter)
     products_filter= filter.qs
+    
+    if request.method== "POST" and "add_to_cart" in request.POST:
+        product_id= request.POST.get("add_to_cart")
+        prod= Products.objects.get(id= product_id)
+        cart= Cart.objects.create(
+            product= prod,
+            price= prod.price
+        )
+        return redirect('Shop:shop')
+
     context= {"products_filter": products_filter,'products' : products, 'top_orders': top_orders, 'carts': carts, 'filter':filter}
     return render(request, 'Shop/category.html', context)
